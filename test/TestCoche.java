7947d41bbe3a80e1db7125e4ca50a6d4de2b0a07
CarlosP <pebeka21@gmail.com>
2023-06-10 05:44:57 +0200

    Una vez seleccionada la opcion test Source Root creamos la clase TestCoche

--- test/TestCoche.java
+++ test/TestCoche.java
@@ -1,10 +1,3 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 public class TestCoche {
-       @Test
-       public void test_crear_coche_con_velocidad_creo(){
-              coche nuevoCoche = new coche();
-              Assertions.assertEquals(0, nuevoCoche.velocidad);
-       }
+
 }
--- test/coche.java
+++
@@ -1,4 +0,0 @@
-public class coche {
-
-    public int velocidad;
-}
