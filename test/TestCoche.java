cf5e959a31971c1d3377858431334903748657ab
CarlosP <pebeka21@gmail.com>
2023-06-10 05:59:46 +0200

    Dentro de ella ya hemos creado nuestro test para crear un coche, utilizando otra clase coche para que lo identifique como simbolo. En esta parte he añadido al metodo mi nombre ya que en las memeorias no estaba puesto

--- test/TestCoche.java
+++ test/TestCoche.java
@@ -1,3 +1,10 @@
-public class TestCoche {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class TestCoche {
+       @Test
+       public void test_crear_coche_CarlosManuel(){
+              coche nuevoCoche = new coche();
+
+       }
 }
